da95ac8ed7b54517f88608eeaa1326a2f1fe13c2
Toci <tocirml2@gmail.com>
2020-11-11 18:04:21 -0600

    Add donate button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,26 @@ const Footer = () => (
       Please dont kill me.
     </p>
     <p className="footer_autor">Still on development. Created by: Sharizart</p>
+    <div className="paypal-button">
+      <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <input type="hidden" name="hosted_button_id" value="BDEEYB76J46HC" />
+        <input
+          type="image"
+          src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"
+          border="0"
+          name="submit"
+          title="PayPal - The safer, easier way to pay online!"
+          alt="Donate with PayPal button"
+        />
+        <img
+          alt=""
+          border="0"
+          src="https://www.paypal.com/en_CR/i/scr/pixel.gif"
+          width="1"
+          height="1"
+        />
+      </form>
+    </div>
   </footer>
 );
 
