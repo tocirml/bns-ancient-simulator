7097c6471911b2ae2c1229e5c56f074bb34e847c
Toci <tocirml2@gmail.com>
2020-11-20 10:16:54 -0600

    Add carousel to bns classes

--- src/components/BnsClasses.js
+++ src/components/BnsClasses.js
@@ -1,24 +1,33 @@
-import bnsClassList from './bnsClassList';
+import bnsClassList from '../data/bnsClassList';
+import Carousel from 'react-elastic-carousel';
 
 const BnsClasses = ({ currentClass, setCurrentClass }) => {
   const classSelectHandler = (bnsClass) => {
     setCurrentClass(bnsClass);
   };
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 3 },
+    { width: 300, itemsToShow: 7 },
+    { width: 650, itemsToShow: 13 },
+  ];
   return (
     <div className="bns__classes">
-      {bnsClassList.map((item) => {
-        return (
-          <div
-            key={Math.random() * 10000}
-            className={`bns__classes-item ${
-              item.name === currentClass ? 'current-class' : ''
-            }`}
-            onClick={() => classSelectHandler(item.name)}
-          >
-            <img src={`/bnsClasses/${item.name}.png`} alt="" />
-          </div>
-        );
-      })}
+      <Carousel breakPoints={breakPoints}>
+        {bnsClassList.map((item) => {
+          return (
+            <div
+              key={Math.random() * 10000}
+              className={`bns__classes-item ${
+                item.name === currentClass ? 'current-class' : ''
+              }`}
+              onClick={() => classSelectHandler(item.name)}
+            >
+              <img src={`/bnsClasses/${item.name}.png`} alt="" />
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 };
